906c56a46eebd192108d806c5ea63799b5609b95
ryzen <mirzaaliuntauhid@gmail.com>
2026-07-04 19:09:06 +0600

    solution

--- leet/1.py
+++ leet/1.py
@@ -1,7 +1,12 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+
+        seen = {}
+
+        for i in range(len(nums)):
+            num = nums[i]
+            complement = target - num
+
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i

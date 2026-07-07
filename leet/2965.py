763d67980b0e050a3ddcf20f7b651dd074415321
Mirza <mirzaaliuntauhid@gmail.com>
2026-07-07 16:20:58 +0600

    solution

---
+++ leet/2965.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        n = len(grid)
+        total = n*n
+        freq = [0] * (total+1)
+
+        for row in grid:
+            for num in row:
+                freq[num] += 1
+
+        repeat = missing = 0
+
+        for num in range(1, total+1):
+            if freq[num] == 2:
+                repeat = num
+            elif freq[num] == 0:
+                missing = num
+
+        return [repeat, missing]

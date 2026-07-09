7e4f0b387fea1a2359a41b7227b3334944310f32
Mirza <mirzaaliuntauhid@gmail.com>
2026-07-09 23:19:17 +0600

    solution

---
+++ leet/2678.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        index = 11
+        count = 0
+        for person in details:
+            age = int(person[11:13])
+            if age > 60:
+                count += 1
+        return count

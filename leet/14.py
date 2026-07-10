49efab26700e37db92977f4185e29675fce51eb4
Mirza <mirzaaliuntauhid@gmail.com>
2026-07-10 12:27:13 +0600

    solution

--- leet/14.py
+++ leet/14.py
@@ -4,4 +4,16 @@ from typing import List
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
 
-        common = ""
+        if not strs:
+            return ""
+        prefix = strs[0]
+
+        for i in strs[1:]:
+            while not i.startswith(prefix):
+
+                prefix = prefix[:-1]
+
+                if not prefix:
+                    return ""
+
+        return prefix

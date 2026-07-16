18e64a2d57f9568384dd6eb443c5cc122c11757c
ryzen <mirzaaliuntauhid@gmail.com>
2026-07-16 15:52:45 +0600

    solution

--- leet/121.py
+++ leet/121.py
@@ -3,3 +3,10 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        min_price = prices[0]
+        max_profit = 0
+        for i in prices:
+            min_price = min(i, min_price)
+            profit = i - min_price
+            max_profit = max(profit, max_profit)
+        return max_profit

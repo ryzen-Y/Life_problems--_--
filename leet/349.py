50ffcf1c58c2ebaf83b8941e8b92af3edfef1c7a
ryzen <mirzaaliuntauhid@gmail.com>
2026-07-17 14:52:03 +0600

    solution2

--- leet/349.py
+++ leet/349.py
@@ -3,10 +3,11 @@ from typing import List
 
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        common = []
-        for i in nums1:
-            for j in nums2:
-                if i == j and i not in common:
-                    common.append(i)
+        seen = set(nums1)
+        ans = set()
 
-        return common
+        for i in nums2:
+            if i in seen:
+                ans.add(i)
+
+        return list(ans)

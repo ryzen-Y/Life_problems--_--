bc393ddfdcc99b58fd0b08e044b189def65bfc48
ryzen <mirzaaliuntauhid@gmail.com>
2026-07-17 12:23:22 +0600

    solution2

--- leet/283.py
+++ leet/283.py
@@ -3,6 +3,9 @@ from typing import List
 
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+
+        count = nums.count(0)
+
+        for _ in range(count):
+            nums.remove(0)
+            nums.append(0)

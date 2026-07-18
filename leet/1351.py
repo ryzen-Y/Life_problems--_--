547f7839e7ae15f430afc334416a9e7d648c1976
ryzen <mirzaaliuntauhid@gmail.com>
2026-07-18 14:27:14 +0600

    solution

--- leet/1351.py
+++ leet/1351.py
@@ -1,4 +1,13 @@
 from typing import List
+
+
 class Solution:
     def countNegatives(self, grid: List[List[int]]) -> int:
-        
+        count = 0
+
+        for _ in grid:
+            for i in _:
+                if i < 0:
+                    count += 1
+
+        return count

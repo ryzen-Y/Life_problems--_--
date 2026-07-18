6d8ad1e1837dc3b494649a460a6c644e93ee86ed
ryzen <mirzaaliuntauhid@gmail.com>
2026-07-18 15:23:24 +0600

    solution2

--- leet/1365.py
+++ leet/1365.py
@@ -3,13 +3,14 @@ from typing import List
 
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        t = sorted(nums)
+        ans = []
+        dic = {}
 
-        out = []
+        for i, val in enumerate(t):
+            if val not in dic:
+                dic[val] = i
+
         for i in nums:
-            count = 0
-            for j in nums:
-                if i > j:
-                    count += 1
-            out.append(count)
-
-        return out
+            ans.append(dic[i])
+        return ans

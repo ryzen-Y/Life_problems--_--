e5c7830131e3034a0582fc97c7ebcbe70710e51a
ryzen <mirzaaliuntauhid@gmail.com>
2026-07-20 16:42:38 +0600

    solution

--- leet/414.py
+++ leet/414.py
@@ -3,3 +3,8 @@ from typing import List
 
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
+        distinct = sorted(set(nums))
+
+        if len(distinct) < 3:
+            return distinct[-1]
+        return distinct[-3]

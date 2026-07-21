a802bb2b38f17d127130be275f9ab17d00b1c30d
ryzen <mirzaaliuntauhid@gmail.com>
2026-07-22 01:29:48 +0600

    solution

--- leet/1480.py
+++ leet/1480.py
@@ -3,3 +3,11 @@ from typing import List
 
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
+
+        ans = 0
+        sum = []
+
+        for i in nums:
+            ans = ans + i
+            sum.append(ans)
+        return sum

a0b0d83c802f3244d75fd1625f93742736e60d1b
ryzen <mirzaaliuntauhid@gmail.com>
2026-07-21 15:26:49 +0600

    solution

--- leet/1929.py
+++ leet/1929.py
@@ -3,3 +3,9 @@ from typing import List
 
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
+        ans = []
+        for i in nums:
+            ans.append(i)
+        for i in nums:
+            ans.append(i)
+        return ans

08543dde8673366f684d5268f830f9e5742b3e4b
ryzen <mirzaaliuntauhid@gmail.com>
2026-07-21 14:53:50 +0600

    solution

--- leet/1920.py
+++ leet/1920.py
@@ -3,3 +3,8 @@ from typing import List
 
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
+        ans = []
+
+        for i in range(len(nums)):
+            ans.append(nums[nums[i]])
+        return ans

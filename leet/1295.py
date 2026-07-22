33fe89679af4b404762c264deae37af6d001155a
ryzen <mirzaaliuntauhid@gmail.com>
2026-07-23 01:07:16 +0600

    solution

--- leet/1295.py
+++ leet/1295.py
@@ -1,5 +1,15 @@
-from typing import List
-
-
 class Solution:
     def findNumbers(self, nums: List[int]) -> int:
+        count = 0
+        digit_list = []
+
+        for i in nums:
+            digit_list = []
+            while i > 0:
+                digit = i % 10
+                i = i // 10
+                digit_list.append(digit)
+
+            if len(digit_list) % 2 == 0:
+                count += 1
+        return count

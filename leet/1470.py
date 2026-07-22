b8f4583ba205362c2da0b7660c936d1a7422aa9b
ryzen <mirzaaliuntauhid@gmail.com>
2026-07-22 21:13:00 +0600

    solution

--- leet/1470.py
+++ leet/1470.py
@@ -4,10 +4,9 @@ from typing import List
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
 
-        num1 = nums[:n]
-        num2 = nums[n:]
         ans = []
-        for x, y in zip(num1, num2):
-            ans.append(x)
-            ans.append(y)
+
+        for x in range(n):
+            ans.append(nums[x])
+            ans.append(nums[n+x])
         return ans

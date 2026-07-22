18465eb81e2fce86fa827ec73f5e1269c3bad891
ryzen <mirzaaliuntauhid@gmail.com>
2026-07-22 23:08:26 +0600

    solution

--- leet/1512.py
+++ leet/1512.py
@@ -3,3 +3,9 @@ from typing import List
 
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
+        count = 0
+        for i in range(len(nums)):
+            for j in nums[i+1:]:
+                if nums[i] == j:
+                    count += 1
+        return count

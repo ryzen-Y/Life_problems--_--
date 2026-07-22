78131d074e076fc7d215be7c290995406030d393
ryzen <mirzaaliuntauhid@gmail.com>
2026-07-23 00:12:41 +0600

    solution

--- leet/1431.py
+++ leet/1431.py
@@ -3,3 +3,12 @@ from typing import List
 
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        maximum = 0
+        result = []
+
+        for i in candies:
+            if i + extraCandies >= max(candies):
+                result.append(True)
+            else:
+                result.append(False)
+        return result

6d0ae6335677c030fcc196f7f28386a9d05cb757
ryzen <mirzaaliuntauhid@gmail.com>
2026-07-23 11:42:07 +0600

    soution

--- leet/217.py
+++ leet/217.py
@@ -1,2 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        flag = False
+        for i in range(len(nums)):
+            if nums[i] in nums[i+1:]:
+                flag = True
+
+        return flag

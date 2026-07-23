82bd212f1041e141f908848528a081c013e1f57f
ryzen <mirzaaliuntauhid@gmail.com>
2026-07-23 11:49:25 +0600

    solution

--- leet/217.py
+++ leet/217.py
@@ -10,3 +10,14 @@ class Solution:
                 return True
             seen.add(i)
         return False
+
+
+# == == == == == == == == = Improved == == == == == == == == ==
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        a = set(nums)
+        if len(a) == len(nums):
+            return False
+        return True

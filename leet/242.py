a880ab01b17f2964381b5635adb6554e67266d5b
ryzen <mirzaaliuntauhid@gmail.com>
2026-07-23 14:32:26 +0600

    solution

--- leet/242.py
+++ leet/242.py
@@ -1,2 +1,22 @@
+from collections import Counter
+from collections import Couters
+
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        a = sorted(s)
+        b = sorted(t)
+
+        if a == b:
+            return True
+        return False
+
+
+# ========== counter =========
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        return Counter(s) == Counter(t)

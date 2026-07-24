ac95f2a2f36d0f6a6e16eee771749b39a6e471ac
ryzen <mirzaaliuntauhid@gmail.com>
2026-07-24 14:36:21 +0600

    solution

--- leet/1207.py
+++ leet/1207.py
@@ -1,5 +1,10 @@
 from typing import List
+from collections import Counter
 
 
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
+        count = Counter(arr)
+        values = list(count.values())
+
+        return len(values) == len(set(values))
